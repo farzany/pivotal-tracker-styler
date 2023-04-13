74f88d1a40bc574406c3f1f1cfd663f09562564b
Adam Conner <adam.conner@coconutsoftware.com>
2023-04-13 11:37:17 -0300

    updated box shaadow to be based on review status

--- content.js
+++ content.js
@@ -42,23 +42,75 @@ function applyUniqueBackgroundColors() {
 
     // Update the owner element's background color
     element.style.backgroundColor = backgroundColor;
-
-    // Add a colored left border to the ticket card
-    const headerElement = element.closest('header');
-    if (headerElement) {
-      headerElement.style.boxShadow = `inset 4px 0 0 0 ${backgroundColor}`;
-    }
   });
 }
+
+function applyBoxShadowColorsBasedOnReviewCount() {
+  const parentDivs = document.querySelectorAll('div[id^="panel_current"]');
+
+  parentDivs.forEach(parentDiv => {
+    const childDivs = parentDiv.querySelectorAll('.StoryPreviewItem__clickToExpand');
+    childDivs.forEach(childDiv => {
+      const reviewList = childDiv.querySelectorAll('[class^="StoryPreviewItemReviewList"]');
+
+      let totalReviews = 0;
+      let approvedCodeReviewCount = 0;
+      let approvedQaReviewCount = 0;
+      reviewList.forEach(review => {
+        const spans = review.querySelectorAll('span');
+        const reviewSpans = Array.from(spans).filter(span => span.parentElement === review);
+        reviewSpans.forEach(reviewSpan => {
+          const imgPassed = reviewSpan.querySelector('img[alt="Pass"]');
+          const span = reviewSpan.querySelector('span[data-aid="StoryPreviewItemReview__reviewType"]');
+
+          totalReviews++;
+
+          if (imgPassed && imgPassed.alt === 'Pass' && span?.textContent === 'Code') {
+            approvedCodeReviewCount++;
+          }
+          if (imgPassed && imgPassed.alt === 'Pass' && span?.textContent === 'Test (QA)') {
+            approvedQaReviewCount++;
+          }
+        });
+      });
+
+      if (totalReviews == 0) {
+        //in progress or unstarted
+        const header = childDiv.querySelector('header');
+        header.style.boxShadow = `inset 10px 0 0 0 #EEEEE4`;
+        //TODO: Separate out in progress and unstarted from current sprint
+      } else {
+        if (approvedCodeReviewCount > 1) {
+          if (approvedQaReviewCount > 0) {
+            //resolved
+            const header = childDiv.querySelector('header');
+            header.style.boxShadow = `inset 10px 0 0 0 #99FC98`;
+          } else {
+            //acceptance
+            const header = childDiv.querySelector('header');
+            header.style.boxShadow = `inset 10px 0 0 0 #FDFD96`;
+          }
+        } else {
+          //code review
+          const header = childDiv.querySelector('header');
+          header.style.boxShadow = `inset 10px 0 0 0 #A3D3E5`;
+        }
+      }
+    });
+  });
+}
+
 
 // Apply unique background colors after the DOM is loaded
 document.addEventListener('DOMContentLoaded', () => {
   applyUniqueBackgroundColors();
+  applyBoxShadowColorsBasedOnReviewCount();
 });
 
 // If the page uses a dynamic framework, listen for changes to the DOM and apply unique background colors again
 const observer = new MutationObserver(debounce(() => {
   applyUniqueBackgroundColors();
+  applyBoxShadowColorsBasedOnReviewCount();
 }, 500));
 
 observer.observe(document.body, {
